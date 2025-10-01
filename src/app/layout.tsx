b7140bf2e725d6dfc5ae37b49f4b9135447f9c70
charbonierlouis <louis.charbonier@gmail.com>
2025-10-01 15:05:31 +0200

    Branche 3 (#4)
    * b3
    
    * toto
    
    ---------
    
    Co-authored-by: Louis Charbonier <louischarbonier@mac.home>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        TITI
         {children}
+        TOTO
       </body>
     </html>
   );
